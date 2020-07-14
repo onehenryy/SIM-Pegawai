a62788fd9dbecd3ec48586418b6313312b0f71d9
onehenryy <41278878+onehenryy@users.noreply.github.com>
2020-07-14 22:44:07 +0700

    1 SIM

---
+++ application/controllers/dashboard_admin.php
@@ -0,0 +1,118 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard_Admin extends CI_Controller {
+
+	/*
+		***	Controller : dashboard_admin.php
+
+	*/
+
+	public function index()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+			$d['judul_lengkap'] = $this->config->item('nama_aplikasi_full');
+			$d['judul_pendek'] = $this->config->item('nama_aplikasi_pendek');
+			$d['instansi'] = $this->config->item('nama_instansi');
+			$d['credit'] = $this->config->item('credit_aplikasi');
+			$d['alamat'] = $this->config->item('alamat_instansi');
+			
+			$page=$this->uri->segment(3);
+			$limit=$this->config->item('limit_data');
+			if(!$page):
+			$offset = 0;
+			else:
+			$offset = $page;
+			endif;
+			
+			$d['tot'] = $offset;
+			$tot_hal = $this->db->get("tbl_data_pegawai");
+			$config['base_url'] = base_url() . 'dashboard_admin/index/';
+			$config['total_rows'] = $tot_hal->num_rows();
+			$config['per_page'] = $limit;
+			$config['uri_segment'] = 3;
+			$config['first_link'] = 'Awal';
+			$config['last_link'] = 'Akhir';
+			$config['next_link'] = 'Selanjutnya';
+			$config['prev_link'] = 'Sebelumnya';
+			$this->pagination->initialize($config);
+			$d["paginator"] =$this->pagination->create_links();
+			
+			$d['data_pegawai'] = $this->db->query("select a.nip, a.nama_pegawai, b.golongan, c.nama_status, a.id_pegawai,u.nama_unit_kerja as 'unit_kerja'
+				from tbl_data_pegawai a 
+			left join tbl_master_golongan b on a.id_golongan=b.id_golongan
+			left join tbl_master_status_pegawai c on a.status_pegawai=c.id_status_pegawai 
+			left join tbl_master_unit_kerja u on u.id_unit_kerja = a.id_unit_kerja
+			limit ".$offset.",".$limit."");
+			
+			$this->load->view('dashboard_admin/home/home',$d);
+		}
+		else
+		{
+			header('location:'.base_url().'');
+		}
+	}
+
+	public function cari()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+			$d['judul_lengkap'] = $this->config->item('nama_aplikasi_full');
+			$d['judul_pendek'] = $this->config->item('nama_aplikasi_pendek');
+			$d['instansi'] = $this->config->item('nama_instansi');
+			$d['credit'] = $this->config->item('credit_aplikasi');
+			$d['alamat'] = $this->config->item('alamat_instansi');
+			
+			if($this->input->post("cari")=="")
+			{
+				$kata = $this->session->userdata('kata');
+			}
+			else
+			{
+				$sess_data['kata'] = $this->input->post("cari");
+				$this->session->set_userdata($sess_data);
+				$kata = $this->session->userdata('kata');
+			}
+			
+			$page=$this->uri->segment(3);
+			$limit=$this->config->item('limit_data');
+			if(!$page):
+			$offset = 0;
+			else:
+			$offset = $page;
+			endif;
+			
+			$d['tot'] = $offset;
+			$tot_hal = $this->db->query("select a.nip, a.nama_pegawai, b.golongan, c.nama_status, a.id_pegawai from tbl_data_pegawai a left join tbl_master_golongan b on a.id_golongan=b.id_golongan
+			left join tbl_master_status_pegawai c on a.status_pegawai=c.id_status_pegawai where a.nama_pegawai like '%".$kata."%' ");
+			$config['base_url'] = base_url() . 'dashboard_admin/index/';
+			$config['total_rows'] = $tot_hal->num_rows();
+			$config['per_page'] = $limit;
+			$config['uri_segment'] = 3;
+			$config['first_link'] = 'Awal';
+			$config['last_link'] = 'Akhir';
+			$config['next_link'] = 'Selanjutnya';
+			$config['prev_link'] = 'Sebelumnya';
+			$this->pagination->initialize($config);
+			$d["paginator"] =$this->pagination->create_links();
+			
+			$d['data_pegawai'] = $this->db->query("select a.nip, a.nama_pegawai, b.golongan, c.nama_status, a.id_pegawai from tbl_data_pegawai a left join tbl_master_golongan b on a.id_golongan=b.id_golongan
+			left join tbl_master_status_pegawai c on a.status_pegawai=c.id_status_pegawai where a.nama_pegawai like '%".$kata."%' limit ".$offset.",".$limit."");
+			
+			$this->load->view('dashboard_admin/home/home',$d);
+		}
+		else
+		{
+			header('location:'.base_url().'');
+		}
+	}
+	
+	function logout()
+	{
+		$this->session->sess_destroy();
+		header('location:'.base_url().'');
+	}
+}
+
+/* End of file dashboard_admin.php */
+/* Location: ./application/controllers/dashboard_admin.php */
